87241da60ab8d7801e0c8a339ed0ce332b320c58
Brice Bartoletti <bartolettibrice@gmail.com>
2020-10-17 17:47:53 +0200

    pset4 - edges ongoing - memory access error -> to debug

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void copy_RGBA_to_RGBB(RGBTRIPLE *rgbA, RGBTRIPLE *rgbB);
+int capped_square_root(int a);
 
 void rgb_swap(RGBTRIPLE *rgbA, RGBTRIPLE *rgbB);
 
@@ -23,31 +23,31 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 }
 
 //// Reflect image horizontally : swapping method : Ok
-//void reflect(int height, int width, RGBTRIPLE image[height][width])
-//{
-//    for (int i = 0; i < height; ++i)
-//    {
-//        for (int j = 0, half_width = width / 2; j < half_width; ++j)
-//        {
-//            rgb_swap(&image[i][j], &image[i][width - 1 - j]);
-//        }
-//    }
-//}
-
-// Reflect image horizontally : Copying the line over the array : Ok
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; ++i)
     {
-        RGBTRIPLE line[width];
-        for (int j = 0; j < width; ++j)
+        for (int j = 0, half_width = width / 2; j < half_width; ++j)
         {
-            line[width - 1 - j] = image[i][j];
+            rgb_swap(&image[i][j], &image[i][width - 1 - j]);
         }
-        RGBTRIPLE *p_image_line = image[i];
-        memcpy((void *) p_image_line, (void *) line, width * sizeof(RGBTRIPLE));
     }
 }
+
+// Reflect image horizontally : Copying the line over the array : Ok
+//void reflect(int height, int width, RGBTRIPLE image[height][width])
+//{
+//    for (int i = 0; i < height; ++i)
+//    {
+//        RGBTRIPLE line[width];
+//        for (int j = 0; j < width; ++j)
+//        {
+//            line[width - 1 - j] = image[i][j];
+//        }
+//        RGBTRIPLE *p_image_line = image[i];
+//        memcpy((void *) p_image_line, (void *) line, width * sizeof(RGBTRIPLE));
+//    }
+//}
 
 //// Blur image : simple method : Ok
 //void blur(int height, int width, RGBTRIPLE image[height][width])
@@ -145,15 +145,15 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int total_blue = 0;
             int total_red = 0;
             int n = 0;
-            for (int l = -1; l <= 1; ++l)
+            for (int k = -1; k <= 1; ++k)
             {
-                for (int m = -1; m <= 1; ++m)
+                for (int l = -1; l <= 1; ++l)
                 {
-                    if (i + l >= 0 && i + l < height && j + m >= 0 && j + m < width)
+                    if (i + k >= 0 && i + k < height && j + l >= 0 && j + l < width)
                     {
-                        total_green += image[i + l][j + m].rgbtGreen;
-                        total_red += image[i + l][j + m].rgbtRed;
-                        total_blue += image[i + l][j + m].rgbtBlue;
+                        total_green += image[i + k][j + l].rgbtGreen;
+                        total_red += image[i + k][j + l].rgbtRed;
+                        total_blue += image[i + k][j + l].rgbtBlue;
                         n++;
                     }
                 }
@@ -170,23 +170,58 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
 
-    return;
+    RGBTRIPLE edge_image[height][width];
+    // Gx and Gy definition
+    int GX[3][3] = {{-1, 0, 1},
+                    {-2, 0, 2},
+                    {-1, 0, 1}};
+    int GY[3][3] = {{-1, -2, -1},
+                    {0,  0,  0},
+                    {1,  2,  1}};
+
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            // compute for this pixel
+            int xRed, yRed, xGreen, yGreen, xBlue, yBlue;
+            xRed = yRed = xGreen = yGreen = xBlue = yBlue = 0;
+            for (int k = -1; k <= 1; ++k)
+            {
+                for (int l = -1; l <= 1; ++l)
+                {
+                    // if border -> Value = 0 -> same as not adding anything
+                    if (i + k >= 0 && i + k < height && j + l >= 0 && j + l < width)
+                    {
+                        xRed += image[i + k][j + l].rgbtRed * GX[k + 1][l + 1];
+                        yRed += image[j + k][j + l].rgbtRed * GY[k + 1][l + 1];
+                        xBlue += image[i + k][j + l].rgbtBlue * GX[k + 1][l + 1];
+                        yBlue += image[j + k][j + l].rgbtBlue * GY[k + 1][l + 1];
+                        xGreen += image[i + k][j + l].rgbtGreen * GX[k + 1][l + 1];
+                        yGreen += image[j + k][j + l].rgbtGreen * GY[k + 1][l + 1];
+                    }
+                }
+            }
+            // define final value for RGB : square_root(x² + y²) capped at 255
+            edge_image[i][j].rgbtRed = capped_square_root(xRed * xRed + yRed * yRed);
+            edge_image[i][j].rgbtGreen = capped_square_root(yGreen * yGreen + yGreen * yGreen);
+            edge_image[i][j].rgbtBlue = capped_square_root(yBlue * yBlue + yBlue * yBlue);
+        }
+    }
+    memcpy((void *) image, (void *) edge_image, height * width * sizeof(RGBTRIPLE));
+}
+
+int capped_square_root(int a)
+{
+    int result = (int) round(sqrt(a));
+    return result > 255 ? 255 : result;
 }
 
 void rgb_swap(RGBTRIPLE *rgbA, RGBTRIPLE *rgbB)
 {
     RGBTRIPLE *swapper = malloc(sizeof(RGBTRIPLE));
-    copy_RGBA_to_RGBB(rgbA, swapper);
-    copy_RGBA_to_RGBB(rgbB, rgbA);
-    copy_RGBA_to_RGBB(swapper, rgbB);
+    memcpy((void *) swapper, (void *) rgbA, sizeof(RGBTRIPLE));
+    memcpy((void *) rgbA, (void *) rgbB, sizeof(RGBTRIPLE));
+    memcpy((void *) rgbB, (void *) swapper, sizeof(RGBTRIPLE));
     free(swapper);
-}
-
-// get reference b and change the value of B
-void copy_RGBA_to_RGBB(RGBTRIPLE *rgbA, RGBTRIPLE *rgbB)
-{
-    // rgb-> is the same than (*rgb).
-    rgbB->rgbtRed = rgbA->rgbtRed;
-    rgbB->rgbtBlue = rgbA->rgbtBlue;
-    rgbB->rgbtGreen = rgbA->rgbtGreen;
 }
