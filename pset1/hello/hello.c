f0e4f1e0d8a9fc36944dbe08ee903c060b57255d
Brice Bartoletti <bartolettibrice@gmail.com>
2020-11-07 00:12:04 +0100

    hello.c - re-implement get_string - ongoing

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-#include <cs50.h>
+//#include <cs50.h>
 
-int main(void)
-{
+int main(void) {
     // Get an input
-    string name = get_string("What is your name?\n");
+//    string name = get_string("What is your name?\n");
+    char name[20];
+    printf("What is your name ?\n");
+    scanf("%s", &name);
+
     // output
     printf("hello, %s\n", name);
+}
+
+char* getStringMax120(char *message){
+    printf("%s", message);
+    char input[120];
+    scanf("%s", &input);
+    int i = 0;
+    while(*(input+i) != "\0")
+        i++;
+    char str[i];
+
 }
