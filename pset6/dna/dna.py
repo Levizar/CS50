85bbb64af52fa31bda983ba559d927b8577e88bd
Brice Bartoletti <bartolettibrice@gmail.com>
2020-12-02 20:50:53 +0100

    pset6 - dna.py - Done, all test succeed !

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -3,48 +3,51 @@ dna.py: This program program that identifies a person based on their DNA.
 """
 __author__ = "Brice Bartoletti"
 
-
-# specs:
-# args:
-#   - arg1 : csv file with str conuts for list of individual
-#   - arg2 : txt file with DNA sequence to identify
-#   X if incorrect len(arg) print an error message with print
-# Open CSV in memory:
-#   first row : name, str1, str2
-#   next row : name, count1, count2
-# Open DNA sequence and read content into memory
-# for each STR of first line CSV file:
-#   compute the longest run of consecutive repeats of str
-# assume one match max
-# if match one individual, print the name
-# else print "No match"
-#
-
 if __name__ == "__main__":
     import sys
     import csv
+    import re
+
     # number of args provided
     if len(sys.argv) != 3:
         print("Usage: python ./dna.py path_to_csv path_to_txt")
     # list of args provided
     _, db_path, txt_path, *kwargs = sys.argv
 
-    # open CSV
+    # Load CSV data in memory then close
     with open(db_path, newline='') as csvFile:
-        lineReader = csv.reader(csvFile)
-        # load CSV into mem
-        for csvRow in lineReader:
-            print(", ".join(csvRow))
+        dnaSeqs, *data = csv.reader(csvFile)
+        csvFile.close()
 
+    # Create dictionary where key are DNA Sequence combination and value = name
+    dictDnaKeyPeopleValue = {}
+    for line in data:
+        key = []
+        for i, colVal in enumerate(line):
+            if i == 0:
+                name = colVal
+            else:
+                key.append(int(colVal))
+        key = tuple(key)
+        dictDnaKeyPeopleValue[key] = name
 
-    # close CSV
+    # Load Txt data into memory then close
+    with open(txt_path) as txtFile:
+        dnaToAnalyse = txtFile.readline()
+        txtFile.close()
 
-    # open Txt
+    # remove the "name" value to only keep the dna sequences
+    dnaSeqs.pop(0)
+    # Analyse dna
+    dnaKey = []
+    # for each sequence in the database
+    for dnaSeq in dnaSeqs:
+        # find all repetition of this sequence
+        seqSize = len(dnaSeq)
+        continueSequences = re.compile(f"({dnaSeq})+").finditer(dnaToAnalyse)
+        dnaKey.append(max([int(len(cs.group())/seqSize) for cs in continueSequences] or [0]))
+    # create the key
+    dnaKey = tuple(dnaKey)
 
-    # load Txt into mem
-
-    # scan Txt for strs longest consecutiv count
-
-    # check for match into csv data
-
-    # return match or nothing
+    # get a result
+    print(dictDnaKeyPeopleValue.get(dnaKey) or "No match")
