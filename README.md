0ee0356e61cafec389f1684bd6f59d22fab53a04
Brice Bartoletti <bartolettibrice@gmail.com>
2020-12-03 23:04:53 +0100

    pset7 - small corrections + readme update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CS50
+
+This repo host the solutions I have made up in order to complete the [CS50 course](https://www.edx.org/course/cs50s-introduction-to-computer-science)
+
+## Remarks:
+
+Coming from a bootcamp after working as an accountant, this course via the learning of the C language taught some basis 
+I was lacking !
+
+The course is very nice and entertaining.
--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT(p2.name), birth
+SELECT DISTINCT(p2.name)
 FROM stars s
          INNER JOIN (
 -- movies for which Kevin Bacon stared
@@ -8,7 +8,7 @@ FROM stars s
     WHERE p.name LIKE 'Kevin Bacon'
       AND birth = 1958
 ) AS kb_starred_movie ON s.movie_id = kb_starred_movie.movie_id
-INNER JOIN people p2 ON p2.id = s.person_id
+         INNER JOIN people p2 ON p2.id = s.person_id
 WHERE (p2.name NOT LIKE 'Kevin Bacon' OR birth != 1958)
 
 
